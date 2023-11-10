c3b16efd5e0a7984382e3b60de965d3c2a8d7670
Brenton Leighton <brenton.leighton@gmail.com>
2023-11-10 12:25:10 +1100

    Move key map functions to key_maps.lua

--- lua/multiple-cursors/init.lua
+++ lua/multiple-cursors/init.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+local key_maps = require("multiple-cursors.key_maps")
 local common = require("multiple-cursors.common")
 local extmarks = require("multiple-cursors.extmarks")
 local virtual_cursors = require("multiple-cursors.virtual_cursors")
@@ -16,10 +17,7 @@ local original_paste_function = nil
 
 local enable_split_paste = true
 
--- A table of key maps
--- mode(s), key(s), function
-local key_maps = {
-
+default_key_maps = {
   -- Left/right motion in normal/visual modes
   {{"n", "x"}, {"h", "<Left>"}, move.normal_h},
   {{"n", "x"}, "<BS>", move_special.normal_bs},
@@ -97,184 +95,8 @@ local key_maps = {
   {"x", "d", visual_mode.d},
 
   -- Escape in all modes
-  {{"n", "i", "x"}, "<Esc>", function() return M.escape() end},
+  {{"n", "i", "x"}, "<Esc>", function() M.escape() end},
 }
-
--- Custom key maps
--- mode(s), key(s), function
-local custom_key_maps = {}
-
--- A table to store any existing key maps so that they can be restored afterwards
--- mode, dict
-local existing_key_maps = {}
-
-local disabled_default_key_maps = {}
-
--- Return x in a table if it isn't already a table
-local function wrap_in_table(x)
-  if type(x) == "table" then
-    return x
-  else
-    return {x}
-  end
-end
-
--- Is the mode and key in custom_key_maps or override default key maps?
-local function is_in_table(t, mode, key)
-
-  if next(t) == nil then
-    return false
-  end
-
-  for i=1, #t do
-    local modes = wrap_in_table(t[i][1])
-    local keys = wrap_in_table(t[i][2])
-
-    for j=1, #modes do
-      for k=1, #keys do
-        if mode == modes[j] and key == keys[k] then
-          return true
-        end
-      end
-    end
-
-  end
-
-  return false
-
-end
-
-local function is_default_key_map_allowed(mode, key)
-  return (not is_in_table(custom_key_maps, mode, key)) and
-      (not is_in_table(disabled_default_key_maps, mode, key))
-end
-
--- Save a single key map
-local function save_existing_key_map(mode, key)
-
-  local dict = vim.fn.maparg(key, mode, false, true)
-
-  -- If the there's a key map
-  if dict["buffer"] ~= nil then
-    -- Add to existing_key_maps
-    table.insert(existing_key_maps, {mode, dict})
-  end
-
-end
-
--- Save any existing key maps
-local function save_existing_key_maps()
-
-  -- Standard key maps
-  for i=1, #key_maps do
-    local modes = wrap_in_table(key_maps[i][1])
-    local keys = wrap_in_table(key_maps[i][2])
-
-    for j=1, #modes do
-      for k=1, #keys do
-        if is_default_key_map_allowed(modes[j], keys[k]) then
-          save_existing_key_map(modes[j], keys[k])
-        end
-      end
-    end
-  end
-
-  -- Custom key maps
-  for i=1, #custom_key_maps do
-    local custom_modes = wrap_in_table(custom_key_maps[i][1])
-    local custom_keys = wrap_in_table(custom_key_maps[i][2])
-
-    for j=1, #custom_modes do
-      for k=1, #custom_keys do
-        save_existing_key_map(custom_modes[j], custom_keys[k])
-      end
-    end
-  end
-
-end
-
--- Restore existing key maps
-local function restore_existing_key_maps()
-
-  for i=1, #existing_key_maps do
-    local mode = existing_key_maps[i][1]
-    local dict = existing_key_maps[i][2]
-
-    vim.fn.mapset(mode, false, dict)
-  end
-
-end
-
--- Set any custom key maps
--- This is a separate function because it's also used by the LazyLoad autocmd
-local function set_custom_key_maps()
-  for i=1, #custom_key_maps do
-    local custom_modes = wrap_in_table(custom_key_maps[i][1])
-    local custom_keys = wrap_in_table(custom_key_maps[i][2])
-    local func = custom_key_maps[i][3]
-
-    for j=1, #custom_modes do
-      for k=1, #custom_keys do
-        vim.keymap.set(custom_modes[j], custom_keys[k], function() normal_edit.custom_function(func) end)
-      end
-    end
-  end
-end
-
--- Set key maps used by this plug-in
-local function set_key_maps()
-
-  -- Standard key maps
-  for i=1, #key_maps do
-    local modes = wrap_in_table(key_maps[i][1])
-    local keys = wrap_in_table(key_maps[i][2])
-    local func = key_maps[i][3]
-
-    for j=1, #modes do
-      for k=1, #keys do
-        if is_default_key_map_allowed(modes[j], keys[k]) then
-          vim.keymap.set(modes[j], keys[k], func)
-        end
-      end
-    end
-  end
-
-  -- Custom key maps
-  set_custom_key_maps()
-
-end
-
--- Delete key maps used by this plug-in
-local function delete_key_maps()
-
-  -- Standard key maps
-  for i=1, #key_maps do
-    local modes = wrap_in_table(key_maps[i][1])
-    local keys = wrap_in_table(key_maps[i][2])
-
-    for j=1, #modes do
-      for k=1, #keys do
-        if is_default_key_map_allowed(modes[j], keys[k]) then
-          vim.keymap.del(modes[j], keys[k])
-        end
-      end
-    end
-  end
-
-  -- Custom key maps
-  for i=1, #custom_key_maps do
-    local custom_modes = wrap_in_table(custom_key_maps[i][1])
-    local custom_keys = wrap_in_table(custom_key_maps[i][2])
-    local func = custom_key_maps[i][3]
-
-    for j=1, #custom_modes do
-      for k=1, #custom_keys do
-        vim.keymap.del(custom_modes[j], custom_keys[k])
-      end
-    end
-  end
-
-end
 
 -- Create autocmds used by this plug-in
 local function create_autocmds()
@@ -318,11 +140,11 @@ local function create_autocmds()
     -- event (when a plugin has been loaded)
     -- This is to fix an issue with using a command from a plugin that was lazy
     -- loaded while multi-cursors is active
-    if next(custom_key_maps) ~= nil then
+    if key_maps.has_custom_keys_maps() then
       vim.api.nvim_create_autocmd({"User"}, {
         group = autocmd_group_id,
         pattern = "LazyLoad",
-        callback = set_custom_key_maps,
+        callback = key_maps.set_custom,
       })
     end
 
@@ -349,8 +171,8 @@ end
 -- Initialise
 local function init()
   if not initialised then
-    save_existing_key_maps()
-    set_key_maps()
+    key_maps.save_existing()
+    key_maps.set()
     create_autocmds()
     override_paste_handler()
 
@@ -362,8 +184,8 @@ end
 local function deinit()
   if initialised then
     virtual_cursors.clear()
-    delete_key_maps()
-    restore_existing_key_maps()
+    key_maps.delete()
+    key_maps.restore_existing()
     vim.api.nvim_clear_autocmds({group = autocmd_group_id}) -- Clear autocmds
     vim.paste = original_paste_function -- Revert the paste handler
 
@@ -421,25 +243,22 @@ end
 
 function M.setup(opts)
 
+  -- Options
   opts = opts or {}
 
-  if opts.enable_split_paste ~= nil then
-    enable_split_paste = opts.enable_split_paste
-  end
+  enable_split_paste = opts.enable_split_paste or true
+  local disabled_default_key_maps = opts.disabled_default_key_maps or {}
+  local custom_key_maps = opts.custom_key_maps or {}
 
-  if opts.disabled_default_key_maps ~= nil then
-    disabled_default_key_maps = opts.disabled_default_key_maps
-  end
-
-  if opts.custom_key_maps ~= nil then
-    custom_key_maps = opts.custom_key_maps
-  end
-
+  -- Set up extmarks
   extmarks.setup()
 
+  -- Set up key maps
+  key_maps.setup(default_key_maps, disabled_default_key_maps, custom_key_maps)
+
+  -- Autocmds
   autocmd_group_id = vim.api.nvim_create_augroup("MultipleCursors", {})
 
-  -- Create commands
   vim.api.nvim_create_user_command("MultipleCursorsAddDown", M.add_cursor_down, {})
   vim.api.nvim_create_user_command("MultipleCursorsAddUp", M.add_cursor_up, {})
   vim.api.nvim_create_user_command("MultipleCursorsMouseAddDelete", M.mouse_add_delete_cursor, {})
---
+++ lua/multiple-cursors/key_maps.lua
@@ -0,0 +1,197 @@
+local M = {}
+
+local normal_edit = require("multiple-cursors.normal_edit")
+
+-- A table of key maps
+-- mode(s), key(s), function
+default_key_maps = {}
+
+-- To disable the default key maps
+-- {mode(s), key(s)}
+disabled_default_key_maps = {}
+
+-- Custom key maps
+-- {mode(s), key(s), function}
+custom_key_maps = {}
+
+-- A table to store any existing key maps so that they can be restored
+-- mode, dict
+local existing_key_maps = {}
+
+function M.setup(_default_key_maps, _disabled_default_key_maps, _custom_key_maps)
+  default_key_maps = _default_key_maps
+  disabled_default_key_maps = _disabled_default_key_maps
+  custom_key_maps = _custom_key_maps
+end
+
+function M.has_custom_keys_maps()
+  return next(custom_key_maps) ~= nil
+end
+
+-- Return x in a table if it isn't already a table
+local function wrap_in_table(x)
+  if type(x) == "table" then
+    return x
+  else
+    return {x}
+  end
+end
+
+-- Is the mode and key in custom_key_maps or override default key maps?
+local function is_in_table(t, mode, key)
+
+  if next(t) == nil then
+    return false
+  end
+
+  for i=1, #t do
+    local modes = wrap_in_table(t[i][1])
+    local keys = wrap_in_table(t[i][2])
+
+    for j=1, #modes do
+      for k=1, #keys do
+        if mode == modes[j] and key == keys[k] then
+          return true
+        end
+      end
+    end
+
+  end
+
+  return false
+
+end
+
+local function is_default_key_map_allowed(mode, key)
+  return (not is_in_table(custom_key_maps, mode, key)) and
+      (not is_in_table(disabled_default_key_maps, mode, key))
+end
+
+-- Save a single key map
+local function save_existing_key_map(mode, key)
+
+  local dict = vim.fn.maparg(key, mode, false, true)
+
+  -- If the there's a key map
+  if dict["buffer"] ~= nil then
+    -- Add to existing_key_maps
+    table.insert(existing_key_maps, {mode, dict})
+  end
+
+end
+
+-- Save any existing key maps
+function M.save_existing()
+
+  -- Default key maps
+  for i=1, #default_key_maps do
+    local modes = wrap_in_table(default_key_maps[i][1])
+    local keys = wrap_in_table(default_key_maps[i][2])
+
+    for j=1, #modes do
+      for k=1, #keys do
+        if is_default_key_map_allowed(modes[j], keys[k]) then
+          save_existing_key_map(modes[j], keys[k])
+        end
+      end
+    end
+  end
+
+  -- Custom key maps
+  for i=1, #custom_key_maps do
+    local custom_modes = wrap_in_table(custom_key_maps[i][1])
+    local custom_keys = wrap_in_table(custom_key_maps[i][2])
+
+    for j=1, #custom_modes do
+      for k=1, #custom_keys do
+        save_existing_key_map(custom_modes[j], custom_keys[k])
+      end
+    end
+  end
+
+end
+
+-- Restore key maps
+function M.restore_existing()
+
+  for i=1, #existing_key_maps do
+    local mode = existing_key_maps[i][1]
+    local dict = existing_key_maps[i][2]
+
+    vim.fn.mapset(mode, false, dict)
+  end
+
+end
+
+-- Set any custom key maps
+-- This is a separate function because it's also used by the LazyLoad autocmd
+function M.set_custom()
+  for i=1, #custom_key_maps do
+    local custom_modes = wrap_in_table(custom_key_maps[i][1])
+    local custom_keys = wrap_in_table(custom_key_maps[i][2])
+    local func = custom_key_maps[i][3]
+
+    for j=1, #custom_modes do
+      for k=1, #custom_keys do
+        vim.keymap.set(custom_modes[j], custom_keys[k], function() normal_edit.custom_function(func) end)
+      end
+    end
+  end
+end
+
+-- Set key maps used by this plug-in
+function M.set()
+
+  -- Default key maps
+  for i=1, #default_key_maps do
+    local modes = wrap_in_table(default_key_maps[i][1])
+    local keys = wrap_in_table(default_key_maps[i][2])
+    local func = default_key_maps[i][3]
+
+    for j=1, #modes do
+      for k=1, #keys do
+        if is_default_key_map_allowed(modes[j], keys[k]) then
+          vim.keymap.set(modes[j], keys[k], func)
+        end
+      end
+    end
+  end
+
+  -- Custom key maps
+  M.set_custom()
+
+end
+
+-- Delete key maps used by this plug-in
+function M.delete()
+
+  -- Default key maps
+  for i=1, #default_key_maps do
+    local modes = wrap_in_table(default_key_maps[i][1])
+    local keys = wrap_in_table(default_key_maps[i][2])
+
+    for j=1, #modes do
+      for k=1, #keys do
+        if is_default_key_map_allowed(modes[j], keys[k]) then
+          vim.keymap.del(modes[j], keys[k])
+        end
+      end
+    end
+  end
+
+  -- Custom key maps
+  for i=1, #custom_key_maps do
+    local custom_modes = wrap_in_table(custom_key_maps[i][1])
+    local custom_keys = wrap_in_table(custom_key_maps[i][2])
+    local func = custom_key_maps[i][3]
+
+    for j=1, #custom_modes do
+      for k=1, #custom_keys do
+        vim.keymap.del(custom_modes[j], custom_keys[k])
+      end
+    end
+  end
+
+end
+
+return M
