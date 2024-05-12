89f2f569ee4f15582b6ee8c580ac386a76eaed1e
Brenton Leighton <brenton.leighton@gmail.com>
2024-05-12 18:21:35 +1000

    Handle case insensitive completion

--- lua/multiple-cursors/insert_mode/completion.lua
+++ lua/multiple-cursors/insert_mode/completion.lua
@@ -6,24 +6,29 @@ local virtual_cursors = require("multiple-cursors.virtual_cursors")
 -- Indicate that the completion word has already been inserted before complete_done_pre
 local completed = false
 
--- Return the completion word without the part that triggered the completion
-local function crop_completion_word(line, col, word)
+-- Get the number of characters to delete before the cursor, and after for
+-- replace mode
+-- Returns num_before, num_after
+local function get_lengths(line, col, word)
+
+  local word_length = word:len()
 
   -- Start from the longest possible length
-  local length = vim.fn.min({col - 1, word:len()})
+  local num_before = vim.fn.min({col - 1, word_length})
 
-  while length > 0 do
-    local l = line:sub(col-length, col-1)
-    local w = word:sub(1, length)
+  while num_before > 0 do
+    local l = line:sub(col-num_before, col-1)
+    local w = word:sub(1, num_before)
 
-    if l == w then
-      return word:sub(length + 1)
+    -- Case insensitive comparison
+    if l:lower() == w:lower() then
+      break
     end
 
-    length = length - 1
+    num_before = num_before - 1
   end
 
-  return word
+  return num_before, (word_length - num_before)
 
 end
 
@@ -43,14 +48,23 @@ function M.complete_if_selected()
 
       -- Remove the part of the word that triggered the completion
       local line = vim.fn.getline(vc.lnum)
-      local cropped_word = crop_completion_word(line, vc.col, word)
 
-      -- Delete characters for replace mode
-      if common.is_mode("R") or common.is_mode("Rc") then
-        vim.cmd("normal! \"_" .. cropped_word:len() .. "x")
+      local num_before, num_after = get_lengths(line, vc.col, word)
+
+      -- Delete any characters before the cursor that belong to the completion word
+      if num_before > 0 then
+        vim.cmd("normal! \"_" .. num_before .. "X")
       end
 
-      vim.api.nvim_put({cropped_word}, "c", false, true)
+      -- Delete characters after the cursor for replace mode
+      if common.is_mode("R") or common.is_mode("Rc") then
+        if num_after > 0 then
+          vim.cmd("normal! \"_" .. num_after .. "x")
+        end
+      end
+
+      -- Put the completion word
+      vim.api.nvim_put({word}, "c", false, true)
 
     end)
 
